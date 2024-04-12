a957742930d80423de303a29db38ea270e14c78b
Abdelsalam <abdonadir500@gmail.com>
2024-04-13 01:39:15 +0200

    Create the product creation view

---
+++ resources/views/admin/products/create.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create Product Page</title>
+</head>
+<body>
+    <form action="{{route('products.store')}}" method="POST">
+        @csrf
+        <input type="text" name="name" value="{{old('name')}}">
+        @error('name')
+            {{$message}}
+        @enderror
+        <input type="text" name="price" value="{{old('price')}}">
+        @error('price')
+            {{$message}}
+        @enderror
+        <select name="category" id="">
+            @foreach ($categories as $category)
+                <option value="{{ $category->id }}">
+                    {{ $category->name }}
+                </option>
+            @endforeach
+        </select>
+        <input type="submit" value="Add">
+    </form>
+</body>
+</html>
