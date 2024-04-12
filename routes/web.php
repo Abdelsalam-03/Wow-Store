b5e3718c0bb644c23ece4a48473a71515fdab923
Abdelsalam <abdonadir500@gmail.com>
2024-04-12 22:24:58 +0200

    Defining the categories resource route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::resource('categories', CategoryController::class);
+// Route::get('/categories', [CategoryController::class, 'index']);
