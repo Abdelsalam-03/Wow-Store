62b888731683291158d242742e154d7d9eb6eb03
Abdelsalam <abdonadir500@gmail.com>
2024-04-13 00:09:07 +0200

    Creating the category store function on the category controller

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -29,7 +29,11 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'unique:categories',],
+        ]);
+        Category::create(['name' => $request->name]);
+        return redirect(route('categories.index'));
     }
 
     /**
