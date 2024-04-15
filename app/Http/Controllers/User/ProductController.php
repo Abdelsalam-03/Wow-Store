e48628380a4207b4ec538572bdb27e6a09f264ca
Abdelsalam <abdonadir500@gmail.com>
2024-04-15 17:24:23 +0200

    Create the show action at the user's product controller

--- app/Http/Controllers/User/ProductController.php
+++ app/Http/Controllers/User/ProductController.php
@@ -11,4 +11,9 @@ class ProductController extends Controller
     function index(){
         return view('user.products.index', ['products' => Product::all()]);
     }
+
+    function show(Request $request){
+        return view('user.products.show', ['product' => Product::findOrFail($request->product)]);
+    }
+
 }
