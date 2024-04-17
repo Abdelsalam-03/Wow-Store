00d3dcf8e838f263cdf5249f739741ce29d9a64b
Abdelsalam <abdonadir500@gmail.com>
2024-04-18 00:56:23 +0200

    Representing the cart content as an html components & adding remove product functionaloty to cart products

--- resources/views/user/products/index.blade.php
+++ resources/views/user/products/index.blade.php
@@ -14,6 +14,10 @@
     <hr>
     <button onclick="destroyCart()">Empty Cart</button>
     <hr>
+    <div class="cart">
+
+    </div>
+    <hr>
     @foreach ($products as $product)
         {{ $product->name }}<br>
         {{ $product->price }}<br>
@@ -23,6 +27,22 @@
         <hr>
     @endforeach
     <script>
+        function cart() {
+            fetch('/cart/all', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                fillCart(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        }
         function addToCart(id) {
             const data = { productId: id };
             fetch('/cart/add', {
@@ -36,6 +56,7 @@
             .then(response => response.json())
             .then(data => {
                 console.log(data);
+                fillSpecificCart(id);
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -53,12 +74,74 @@
                 .then(response => response.json())
                 .then(data => {
                     console.log(data);
+                    document.querySelector('.cart').innerHTML = '';
                 })
                 .catch(error => {
                     console.error('Error:', error);
                 });
             }
         }
+        function fillCart(content) {
+            cartElement = document.querySelector('.cart');
+            content.forEach(element => {
+                let component = document.createElement('div');                
+                let id = document.createElement('p');                
+                let quantity = document.createElement('p');
+                let removeBotton = document.createElement('button');
+                removeBotton.addEventListener('click', () => remove(element.product_id));
+                quantity.className = 'quantity';
+                id.innerHTML = 'Name : ' + element.product_id;
+                quantity.innerHTML = element.quantity;
+                removeBotton.innerHTML = 'Remove';
+                component.id = element.product_id + 'cart';
+                component.append(id);
+                component.append(quantity);
+                component.append(removeBotton);
+                component.append(document.createElement('hr'));
+                cartElement.appendChild(component);
+            });
+        }
+        function fillSpecificCart(productId) {
+            element = document.getElementById(productId + 'cart');
+            if (element) {
+                element.querySelector('.quantity').innerHTML = (+element.querySelector('.quantity').innerHTML + 1);
+            } else {
+                cartElement = document.querySelector('.cart');
+                let component = document.createElement('div');                
+                let id = document.createElement('p');                
+                let quantity = document.createElement('p');
+                let removeBotton = document.createElement('button');
+                removeBotton.addEventListener('click', () => remove(productId));
+                quantity.className = 'quantity';
+                id.innerHTML = 'Name : ' + productId;
+                quantity.innerHTML = 1;
+                removeBotton.innerHTML = 'Remove';
+                component.id = productId + 'cart';
+                component.append(id);
+                component.append(quantity);
+                component.append(removeBotton);
+                component.append(document.createElement('hr'));
+                cartElement.appendChild(component);
+            }
+        }
+        function remove(id) {
+            fetch('/cart/' + id, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                document.getElementById(id + 'cart').remove();
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        }
+        cart();
     </script>
 </body>
 </html>
