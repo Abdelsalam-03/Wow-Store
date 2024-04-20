d206457d22ab0983ead929b04cbf5a77b99705b4
Abdelsalam <abdonadir500@gmail.com>
2024-04-21 00:21:42 +0200

    Create Home Contollrt

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,16 @@ use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Admin\ProductController as AdminProductController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RedirectController;
 use App\Http\Middleware\admin;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/redirect', RedirectController::class)->name('redirect');
 
