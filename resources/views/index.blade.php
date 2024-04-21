8397242e3ee94c5ea0643b0735c10b24108aed73
Abdelsalam <abdonadir500@gmail.com>
2024-04-21 05:02:20 +0200

    modifying the product component and adding products to users index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,26 @@
             <input type="text" name="query" id="">
             <input type="submit" value="Search">
         </form>
-    </x-slot>    
-    <button onclick="destroyCart()">Empty Cart</button>
-    <hr>
-    <div class="cart"></div>
-    <hr>
-    <div class="container px-4 px-lg-5 mt-5">
-        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 row-cols-xxl-5 justify-content-center">
-        @foreach ($products as $product)
-            <x-product :product="$product" />
-        @endforeach
+    </x-slot>
+    <div style="display: none">
+        <button onclick="destroyCart()">Empty Cart</button>
+        <hr>
+        <div class="cart" >
+        </div>
+        <hr>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            
+            <div class="p-4 bg-white rounded shadow">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 justify-content-center">
+                    @foreach ($products as $product)
+                        <x-product :product="$product" />
+                    @endforeach
+                </div>
+            </div>
+            
         </div>
     </div>
     <script>
