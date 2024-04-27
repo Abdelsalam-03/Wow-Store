b3b6b103b0688ac1165c0814e1792ce6068e29da
Abdelsalam <abdonadir500@gmail.com>
2024-04-27 04:45:46 +0300

    Defining the Admin Orders Routes

---
+++ app/Http/Controllers/Api/V1/Admin/OrderController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class OrderController extends Controller
+{
+    function index(Request $request){
+        return response()->json(['orders' => $orders = Order::filter($request)->get()]);
+    }
+
+    public function show(string $id)
+    {
+        $order = Order::find($id);
+        if (!$order) {
+            return response()->json(['message'=>'Not Found'], 400);
+        }
+        $orderProducts = DB::table('order_products')
+        ->join('products', 'products.id', '=', 'order_products.product_id')
+        ->select('order_products.*', 'products.name', 'products.stock', 'products.price as actual_price')
+        ->where('order_products.order_id', '=', $order->id)
+        ->get();
+        return response()->json(['order' => $order, 'orderProducts' => $orderProducts]);
+    }
+
+    public function cancel(string $id)
+    {
+        $order = Order::find($id);
+        if (!$order) {
+            return response()->json(['message' => 'Not Found'], 400);
+        }
+        if ($order->status == 'pending') {
+            $order->status = 'canceled';
+        } else {
+            $products = $order->products;
+            foreach ($products as $product) {
+                $stock = Product::find($product->product_id);
+                $stock->stock = $stock->stock + $product->quantity;
+                $stock->save();
+            }
+        }
+        $order->save();
+
+        return response()->json(['success' => 'Order Canceled']);
+
+    }
+
+    public function process(string $id)
+    {
+        $order = Order::find($id);
+        if (!$order) {
+            return response()->json(['message' => 'Not Found'], 400);
+        }
+        $order->status = 'processing';
+        $order->save();
+
+        $products = $order->products;
+
+        foreach ($products as $product) {
+            $stock = Product::find($product->product_id);
+            $stock->stock = $stock->stock - $product->quantity;
+            $stock->save();
+        }
+
+        return response()->json(['success' => 'Processed']);
+    
+    }
+
+    public function ship(string $id)
+    {
+        $order = Order::find($id);
+        if (!$order) {
+            return response()->json(['message' => 'Not Found'], 400);
+        }
+        $order->status = 'shipped';
+        $order->save();
+
+        return response()->json(['success' => 'shipped']);
+        
+    }
+
+    public function arrive(string $id)
+    {
+        $order = Order::find($id);
+        if (!$order) {
+            return response()->json(['message' => 'Not Found'], 400);
+        }
+        $order->status = 'delivered';
+        $order->save();
+
+        
+        return response()->json(['success' => 'Arrived']);
+        
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Admin\OrderController as AdminOrderController;
 use App\Http\Controllers\Api\V1\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Api\V1\Admin\ProductController as AdminProductController;
 use App\Http\Controllers\Api\V1\CategoryController;
@@ -18,23 +19,22 @@ Route::prefix('v1')->group(function(){
     Route::get('/products/{product}', [ProductController::class, 'show']);
     Route::get('/categories', [CategoryController::class, 'all']);
 
-    Route::apiResource('/categories', AdminCategoryController::class);
-    Route::apiResource('/products', AdminProductController::class);
+    Route::prefix('admin')->group(function(){
+        Route::apiResource('/categories', AdminCategoryController::class);
+        Route::apiResource('/products', AdminProductController::class);
+        Route::get('/orders', [AdminOrderController::class, 'index']);
+        Route::get('/orders/{order}', [AdminOrderController::class, 'show']);
+        Route::delete('/orders/cancel/{order}', [AdminOrderController::class, 'cancel']);
+        Route::patch('/orders/process/{order}', [AdminOrderController::class, 'process']);
+        Route::patch('/orders/ship/{order}', [AdminOrderController::class, 'ship']);
+        Route::patch('/orders/arrive/{order}', [AdminOrderController::class, 'arrive'])->name('orders.arrive');
+
+    });
 
 });
 
 
 // Route::prefix('admin/')->name('admin.')->group(function(){
-//     Route::get('/', AdminHomeController::class)->name('home');
-//     Route::resource('/categories', AdminCategoryController::class);
-//     Route::resource('/products', AdminProductController::class);
-//     Route::get('/orders', [AdminOrderController::class, 'index'])->name('orders');
-//     Route::get('/orders/{order}', [AdminOrderController::class, 'show'])->name('orders.show');
-//     Route::delete('/orders/cancel/{order}', [AdminOrderController::class, 'cancel'])->name('orders.delete');
-//     Route::patch('/orders/process/{order}', [AdminOrderController::class, 'process'])->name('orders.process');
-//     Route::patch('/orders/ship/{order}', [AdminOrderController::class, 'ship'])->name('orders.ship');
-//     Route::patch('/orders/arrive/{order}', [AdminOrderController::class, 'arrive'])->name('orders.arrive');
-//     Route::post('/orders/search', [AdminOrderController::class, 'search'])->name('orders.search');
 //     Route::get('/settings', [AdminSettingsController::class, 'view'])->name('settings');
 //     Route::get('/settings/create', [AdminSettingsController::class, 'create'])->name('settings.create');
 //     Route::post('/settings/set', [AdminSettingsController::class, 'set'])->name('settings.set');
